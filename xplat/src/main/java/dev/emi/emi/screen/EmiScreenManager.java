8ccd782f4e98d59fdc57ec07d7b695aa56e05f95
a.o.glazkov <a.o.glazkov@aspireteam.ru>
2025-12-27 15:48:52 +0300

    Improve recipe tree handling and bookmark usability
    Add `EmiApi.viewRecipeTree()` for viewing recipe trees upon bookmark application. Prevent favoriting `SearchEmiIngredient` items. Adjust `TreeBookmarkNameScreen` rendering to focus the name field by default and fix text rendering order.

--- xplat/src/main/java/dev/emi/emi/screen/EmiScreenManager.java
+++ xplat/src/main/java/dev/emi/emi/screen/EmiScreenManager.java
@@ -1088,6 +1088,7 @@ public class EmiScreenManager {
   							EmiTreeBookmarks.removeBookmark(treeBookmark);
   						} else {
   							EmiTreeBookmarks.apply(treeBookmark);
+							EmiApi.viewRecipeTree();
   						}
   						return true;
   					}
@@ -1264,7 +1265,7 @@ public class EmiScreenManager {
 			} else if (function.apply(EmiConfig.viewUses)) {
 				EmiApi.displayUses(ingredient);
 				return true;
-			} else if (function.apply(EmiConfig.favorite)) {
+			} else if (function.apply(EmiConfig.favorite) && !(ingredient instanceof SearchEmiIngredient)) {
 				EmiFavorites.addFavorite(ingredient, stack.getRecipeContext());
 				repopulatePanels(SidebarType.FAVORITES);
 				return true;
