291ab9fc47f1a6cc80c4479eb3e257b42c99790b
Alexander <78122700@mail.ru>
2026-05-21 10:18:47 +0300

    Merge pull request #1 from Mango-Clark/1.20.1-mango
    Add root grid paging

--- xplat/src/main/java/dev/emi/emi/bom/BoM.java
+++ xplat/src/main/java/dev/emi/emi/bom/BoM.java
@@ -208,16 +208,7 @@ public class BoM {
 		treeIndex = Math.max(0, Math.min(index, trees.size() - 1));
 	}
 
-   	public static void cycleTree(int delta) {
-		if (trees.isEmpty()) {
-			treeIndex = -1;
-			return;
-		}
-		int size = trees.size();
-		treeIndex = ((treeIndex + delta) % size + size) % size;
-	}
-
-   	public static void removeTree(int index) {
+	public static void removeTree(int index) {
 		if (index < 0 || index >= trees.size()) {
 			return;
 		}
@@ -232,6 +223,17 @@ public class BoM {
 	}
 
 	public static void addResolution(EmiIngredient ingredient, EmiRecipe recipe) {
+		addResolution(ingredient, recipe, false);
+	}
+
+	public static void addResolution(EmiIngredient ingredient, EmiRecipe recipe, boolean allTrees) {
+		if (allTrees) {
+			List<MaterialTree> trees = getTrees();
+			for (MaterialTree tree : trees) {
+				tree.addResolution(ingredient, recipe);
+			}
+			return;
+		}
 		MaterialTree tree = getTree();
 		if (tree != null) {
 			tree.addResolution(ingredient, recipe);
