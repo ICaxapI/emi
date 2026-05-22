90e5979b6eaaf071ec70263fd3c59beffa5f60e3
a.o.glazkov <a.o.glazkov@aspireteam.ru>
2026-05-22 23:43:27 +0300

    Refactor tree bookmark logic: centralize handling in `TreeBookmarkNameScreen` and simplify `EmiScreenManager` integration.

--- xplat/src/main/java/dev/emi/emi/screen/EmiScreenManager.java
+++ xplat/src/main/java/dev/emi/emi/screen/EmiScreenManager.java
@@ -1151,6 +1151,11 @@ public class EmiScreenManager {
 		if (hasFocusedTextField(client.currentScreen, 10)) {
 			return false;
 		}
+		if (EmiConfig.addTreeBookmark.matchesKey(keyCode, scanCode)) {
+			if (TreeBookmarkNameScreen.openCurrentTreeBookmark(client.currentScreen)) {
+				return true;
+			}
+		}
 		if (EmiApi.isCheatMode() && EmiConfig.deleteCursorStack.matchesKey(keyCode, scanCode)) {
 			if (deleteCursor(lastMouseX, lastMouseY)) {
 				return true;
