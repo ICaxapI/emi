c1accbf0816837def0d934274c6985ddc5b038a3
a.o.glazkov <a.o.glazkov@aspireteam.ru>
2026-08-22 15:39:28 +0300

    Fix NeoForge dedicated server startup

--- neoforge/src/main/java/dev/emi/emi/platform/neoforge/EmiPacketHandler.java
+++ neoforge/src/main/java/dev/emi/emi/platform/neoforge/EmiPacketHandler.java
@@ -1,6 +1,5 @@
 package dev.emi.emi.platform.neoforge;
 
-import dev.emi.emi.EmiPort;
 import dev.emi.emi.network.CommandS2CPacket;
 import dev.emi.emi.network.CreateItemC2SPacket;
 import dev.emi.emi.network.EmiChessPacket;
@@ -12,12 +11,13 @@ import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.codec.PacketDecoder;
 import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
 public class EmiPacketHandler {
-    private static final CustomPayload.Id<EmiChessPacket.S2C> ID_CHESS_CLIENTBOUND = new CustomPayload.Id<EmiChessPacket.S2C>(EmiPort.id("emi:chess_s2c"));
-    private static final CustomPayload.Id<EmiChessPacket.C2S> ID_CHESS_SERVERBOUND = new CustomPayload.Id<EmiChessPacket.C2S>(EmiPort.id("emi:chess_c2s"));
+    private static final CustomPayload.Id<EmiChessPacket.S2C> ID_CHESS_CLIENTBOUND = new CustomPayload.Id<EmiChessPacket.S2C>(Identifier.of("emi:chess_s2c"));
+    private static final CustomPayload.Id<EmiChessPacket.C2S> ID_CHESS_SERVERBOUND = new CustomPayload.Id<EmiChessPacket.C2S>(Identifier.of("emi:chess_c2s"));
 
     public static void init(RegisterPayloadHandlersEvent event) {
         var registrar = event.registrar("emi").optional();
--- xplat/src/main/java/dev/emi/emi/network/EmiNetwork.java
+++ xplat/src/main/java/dev/emi/emi/network/EmiNetwork.java
@@ -3,16 +3,16 @@ package dev.emi.emi.network;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-import dev.emi.emi.EmiPort;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
 
 public class EmiNetwork {
-	public static final CustomPayload.Id<FillRecipeC2SPacket> FILL_RECIPE = new CustomPayload.Id<FillRecipeC2SPacket>(EmiPort.id("emi:fill_recipe"));
-	public static final CustomPayload.Id<CreateItemC2SPacket> CREATE_ITEM = new CustomPayload.Id<CreateItemC2SPacket>(EmiPort.id("emi:create_item"));
-	public static final CustomPayload.Id<CommandS2CPacket> COMMAND = new CustomPayload.Id<CommandS2CPacket>(EmiPort.id("emi:command"));
-	public static final CustomPayload.Id<EmiChessPacket> CHESS = new CustomPayload.Id<EmiChessPacket>(EmiPort.id("emi:chess"));
-	public static final CustomPayload.Id<PingS2CPacket> PING = new CustomPayload.Id<PingS2CPacket>(EmiPort.id("emi:ping"));
+	public static final CustomPayload.Id<FillRecipeC2SPacket> FILL_RECIPE = new CustomPayload.Id<FillRecipeC2SPacket>(Identifier.of("emi:fill_recipe"));
+	public static final CustomPayload.Id<CreateItemC2SPacket> CREATE_ITEM = new CustomPayload.Id<CreateItemC2SPacket>(Identifier.of("emi:create_item"));
+	public static final CustomPayload.Id<CommandS2CPacket> COMMAND = new CustomPayload.Id<CommandS2CPacket>(Identifier.of("emi:command"));
+	public static final CustomPayload.Id<EmiChessPacket> CHESS = new CustomPayload.Id<EmiChessPacket>(Identifier.of("emi:chess"));
+	public static final CustomPayload.Id<PingS2CPacket> PING = new CustomPayload.Id<PingS2CPacket>(Identifier.of("emi:ping"));
 	private static BiConsumer<ServerPlayerEntity, EmiPacket> clientSender;
 	private static Consumer<EmiPacket> serverSender;
 
